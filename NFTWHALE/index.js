51f709cf521cc9d40e2a94ab78773957702fcff7
sharaba_xo <63738475+sharana-sabesan@users.noreply.github.com>
2024-07-16 11:56:20 -0700

    Add files via upload

---
+++ NFTWHALE/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MoralisProvider appId="PEUbbxutnQ8Aci603DsHvQjeicjDpC8zpvbZbloG" serverUrl="https://1amqtgnam6di.usemoralis.com:2053/server">
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </MoralisProvider>
+    
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
